0bc5ce4c3afe0282eac7c97adbedc506e2f0e944
Ivan Ivanov <aydikstudio@mail.ru>
2024-06-22 18:43:13 +0300

    first commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = () => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
-    }, [tg.MainButton]);
+    }, [tg]);
 
     useEffect(() => {
         if(!city || !country) {
